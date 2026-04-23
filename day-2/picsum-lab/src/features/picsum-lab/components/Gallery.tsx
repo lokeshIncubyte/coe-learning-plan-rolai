29a52fe3bbc2da58a71b9601d4d76bcbd286a431
unknown <lokesh@incubyte.co>
2026-04-23 17:57:16 +0530

    refactor: separate row selection prop drilling from Gallery state

--- day-2/picsum-lab/src/features/picsum-lab/components/Gallery.tsx
+++ day-2/picsum-lab/src/features/picsum-lab/components/Gallery.tsx
@@ -1,6 +1,8 @@
 import type { PicsumPhoto } from '../model/types'
 import { GalleryRow } from './GalleryRow'
 
+type GalleryRowPhoto = Pick<PicsumPhoto, 'id' | 'author' | 'download_url'>
+
 type GalleryProps = {
   photos: PicsumPhoto[]
   selectedPhotoId: string | null
@@ -10,19 +12,23 @@ type GalleryProps = {
 export function Gallery({ photos, selectedPhotoId, onSelectPhoto }: GalleryProps) {
   return (
     <ul>
-      {photos.map((photo) => (
-        <li key={photo.id}>
-          <GalleryRow
-            photo={{
-              id: photo.id,
-              author: photo.author,
-              download_url: photo.download_url,
-            }}
-            isSelected={selectedPhotoId === photo.id}
-            onSelectPhoto={() => onSelectPhoto(photo)}
-          />
-        </li>
-      ))}
+      {photos.map((photo) => {
+        const rowPhoto: GalleryRowPhoto = {
+          id: photo.id,
+          author: photo.author,
+          download_url: photo.download_url,
+        }
+
+        return (
+          <li key={photo.id}>
+            <GalleryRow
+              photo={rowPhoto}
+              isSelected={selectedPhotoId === photo.id}
+              onSelectPhoto={() => onSelectPhoto(photo)}
+            />
+          </li>
+        )
+      })}
     </ul>
   )
 }
