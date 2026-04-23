a4434ebf85a76588b043f4b6e80431716999d63a
unknown <lokesh@incubyte.co>
2026-04-23 19:53:43 +0530

    green: append ?blur when blur effect is on without amount

--- day-2/picsum-lab/src/features/picsum-lab/model/buildPicsumImageUrl.ts
+++ day-2/picsum-lab/src/features/picsum-lab/model/buildPicsumImageUrl.ts
@@ -4,15 +4,20 @@ const PICSUM_BASE_URL = 'https://picsum.photos'
 
 export function buildPicsumImageUrl(input: BuildImageUrlInput): string {
   const { source, width, height, effects } = input
-  const grayscaleQuery = effects.grayscale ? '?grayscale' : ''
+  let query = ''
+  if (effects.grayscale) {
+    query = '?grayscale'
+  } else if (effects.blur) {
+    query = '?blur'
+  }
 
   switch (source.kind) {
     case 'id':
-      return `${PICSUM_BASE_URL}/id/${source.id}/${width}/${height}${grayscaleQuery}`
+      return `${PICSUM_BASE_URL}/id/${source.id}/${width}/${height}${query}`
     case 'seed':
-      return `${PICSUM_BASE_URL}/seed/${source.seed}/${width}/${height}${grayscaleQuery}`
+      return `${PICSUM_BASE_URL}/seed/${source.seed}/${width}/${height}${query}`
     case 'random':
-      return `${PICSUM_BASE_URL}/${width}/${height}${grayscaleQuery}`
+      return `${PICSUM_BASE_URL}/${width}/${height}${query}`
     default: {
       const _exhaustiveCheck: never = source
       return _exhaustiveCheck
