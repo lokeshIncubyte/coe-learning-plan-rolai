de7fe340412c38571faa51f1846e623eb72e714c
unknown <lokesh@incubyte.co>
2026-04-24 17:22:06 +0530

    green: GalleryRow renders a thumbnail image

--- day-2/picsum-lab/src/features/picsum-lab/components/GalleryRow.tsx
+++ day-2/picsum-lab/src/features/picsum-lab/components/GalleryRow.tsx
@@ -1,3 +1,4 @@
+import { buildPicsumImageUrl } from '../model/buildPicsumImageUrl'
 import type { PicsumPhoto } from '../model/types'
 
 type GalleryRowPhoto = Pick<PicsumPhoto, 'id' | 'author' | 'download_url'>
@@ -9,12 +10,20 @@ type GalleryRowProps = {
 }
 
 export function GalleryRow({ photo, isSelected = false, onSelectPhoto }: GalleryRowProps) {
+  const thumbnailUrl = buildPicsumImageUrl({
+    source: { kind: 'id', id: photo.id },
+    width: 160,
+    height: 120,
+    effects: { grayscale: false, blur: false },
+  })
+
   return (
     <button
       type="button"
       aria-pressed={isSelected}
       onClick={() => onSelectPhoto(photo)}
     >
+      <img src={thumbnailUrl} alt={`Photo ${photo.id}`} />
       <span>{photo.id}</span>
       <span>{photo.author}</span>
     </button>
